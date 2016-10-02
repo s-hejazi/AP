df605e4d79aee4a44464ceb546be442b209be72d
samudhraaG <gsamudhraa@gmail.com>
2016-10-02 17:15:00 -0400

    Update AntiPatternHandler.java

--- src/antipatterndetection/handlers/AntiPatternHandler.java
+++ src/antipatterndetection/handlers/AntiPatternHandler.java
@@ -12,11 +12,8 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -25,95 +22,138 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.DoStatement;
 import org.eclipse.jdt.core.dom.EnhancedForStatement;
 import org.eclipse.jdt.core.dom.ForStatement;
-import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.WhileStatement;
 
 
 public class AntiPatternHandler extends AbstractHandler {
-    
+
 	ASTParser parser = ASTParser.newParser(AST.JLS3);
 	//TODO : substitute later, temporary function
 	ArrayList <String> dbMethods = new ArrayList<>();
 	//
-	
+	/*private List<MethodDeclaration> getterMethods = new ArrayList<MethodDeclaration>(); 
+	private List<MethodDeclaration> setterMethods = new ArrayList<MethodDeclaration>();*/ 
+	private List<String> getterMethodNames = new ArrayList<String>();
+	private List<String> setterMethodNames = new ArrayList<String>();
+
+	public static final String GETTER_PREFIX = "get"; 
+	public static final String SETTER_PREFIX = "set"; 
 	public AntiPatternHandler() {
 	}
 
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		//TODO remove later
-	    dbMethods.add("writeResult");
-	    dbMethods.add("setVertice");
-	    //
+		dbMethods.add("writeResult");
+		dbMethods.add("setVertice");
+		//
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
-        IWorkspace workspace = ResourcesPlugin.getWorkspace();
-        IWorkspaceRoot root = workspace.getRoot();
-        // Get all projects in the workspace
-        IProject[] projects = root.getProjects();
-        // Loop over all projects
-        for (IProject project : projects) {
-                try {
-                        if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
+		// Get all projects in the workspace
+		IProject[] projects = root.getProjects();
+		// Loop over all projects
+		for (IProject project : projects) {
+			try {
+				if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
 
-                                IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
-                                for (IPackageFragment mypackage : packages) {
-                                        if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
-                                              for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
-                                                	    // create the AST for the ICompilationUnits
-                                                        parse(unit);
-                                                }
-                                        }
-                                }
-                        }
-                } catch (CoreException e) {
-                        e.printStackTrace();
-                }
-        }
-        return null;
-}
+					IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
+					for (IPackageFragment mypackage : packages) {
+						if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+							for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
+								// create the AST for the ICompilationUnits
+								
+								parseForGetterSetter(unit);
+								parse(unit);
+							}
+						}
+					}
+				}
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
 
 
-/**
- * Reads a ICompilationUnit and creates the AST DOM for manipulating the
- * Java source file
- *
- * @param unit
- * @return
- */
+	/**
+	 * Reads a ICompilationUnit and creates the AST DOM for manipulating the
+	 * Java source file
+	 *
+	 * @param unit
+	 * @return
+	 */
 
-private void parse(ICompilationUnit unit) {
+	private void parse(ICompilationUnit unit) {
 
-        parser.setSource(unit);
-        parser.setResolveBindings(true);
-        final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-        cu.accept(new ASTVisitor(){
-        	public boolean visit (ForStatement node) {
-        		for(String s: dbMethods)
-        		if(node.getBody().toString().contains(s))
-        		System.out.println("Instance found in "+ cu.getJavaElement().getElementName()+ ", For loop in line "+cu.getLineNumber(node.getStartPosition()));       		       		
-        		return true;
-        	}
-          	public boolean visit (WhileStatement node) {
-        		for(String s: dbMethods)
-        		if(node.getBody().toString().contains(s))
-        		System.out.println("Instance found in "+cu.getJavaElement().getElementName()+ " : While loop in line "+cu.getLineNumber(node.getStartPosition()));       		
-        		return true;
-        	}
-          	public boolean visit (EnhancedForStatement node) {
-        		for(String s: dbMethods)
-        		if(node.getBody().toString().contains(s))
-        		System.out.println("Instance found in "+cu.getJavaElement().getElementName()+ " : Enhanced For loop in line "+cu.getLineNumber(node.getStartPosition()));       		
-        		return true;
-        	}
-          	public boolean visit (DoStatement node) {
-        		for(String s: dbMethods)
-        		if(node.getBody().toString().contains(s))
-        		System.out.println("Instance found in "+cu.getJavaElement().getElementName()+ " : Do While loop in line "+cu.getLineNumber(node.getStartPosition()));       		
-        		return true;
-          	}
-        });
-}
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		fillDbMethodsListWithGetterSetterMethodNames();
+		
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+		cu.accept(new ASTVisitor(){
+			public boolean visit (ForStatement node) {
+				for(String s: dbMethods)
+					if(node.getBody().toString().contains(s))
+						System.out.println("Instance found in "+ cu.getJavaElement().getElementName()+ ", For loop in line "+cu.getLineNumber(node.getStartPosition()));       		       		
+				return true;
+			}
+			public boolean visit (WhileStatement node) {
+				for(String s: dbMethods)
+					if(node.getBody().toString().contains(s))
+						System.out.println("Instance found in "+cu.getJavaElement().getElementName()+ " : While loop in line "+cu.getLineNumber(node.getStartPosition()));       		
+				return true;
+			}
+			public boolean visit (EnhancedForStatement node) {
+				for(String s: dbMethods)
+					if(node.getBody().toString().contains(s))
+						System.out.println("Instance found in "+cu.getJavaElement().getElementName()+ " : Enhanced For loop in line "+cu.getLineNumber(node.getStartPosition()));       		
+				return true;
+			}
+			public boolean visit (DoStatement node) {
+				for(String s: dbMethods)
+					if(node.getBody().toString().contains(s))
+						System.out.println("Instance found in "+cu.getJavaElement().getElementName()+ " : Do While loop in line "+cu.getLineNumber(node.getStartPosition()));       		
+				return true;
+			}
+		});
+	}
 
+	private void parseForGetterSetter(ICompilationUnit unit) {
+
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+		cu.accept(new ASTVisitor(){
+			public boolean visit(MethodDeclaration node) { 
+				if(isGetterMethod(node)) 
+					getterMethodNames.add(node.getName().getFullyQualifiedName()); 
+
+				if(isSetterMethod(node)) 
+					setterMethodNames.add(node.getName().getFullyQualifiedName()); 
+
+				return true; 
+			}			
+			
+		});
+	}
+	
+	private void fillDbMethodsListWithGetterSetterMethodNames(){
+		dbMethods.addAll(getterMethodNames);
+		dbMethods.addAll(setterMethodNames);
+	}
+
+	private boolean isGetterMethod(MethodDeclaration methodDeclaration) { 
+		return methodDeclaration.getName().getFullyQualifiedName().startsWith(GETTER_PREFIX); 
+	} 
+
+	private boolean isSetterMethod(MethodDeclaration methodDeclaration) { 
+		return methodDeclaration.getName().getFullyQualifiedName().startsWith(SETTER_PREFIX); 
+	} 
+	
+	
 
 }
 
